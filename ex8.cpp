b82fb97e5d082178accbce6538c83e4ad417aec3
Mikhail Filimovich <m.s.filimovich@gmail.com>
2023-11-28 00:43:26 +0300

    Инициализация. Готов вариант для работы с рекурсией. Реализовать вариант работы через обычный цикл.

---
+++ ex8.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <cmath> // Для round();
+
+using namespace std;
+
+double calculate_row_y(double num)
+{
+	if (num == 0)
+		return 1 / (1 + 1 / 2.0); // 2.0, чтобы вычисления производились с учетом FloatingPoint.;
+	return (1 / (num + calculate_row_y(num - 1)));
+}
+
+double get_num()
+{
+	double num;
+
+	do {
+		cin >> num;
+
+		if (cin.fail()) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "Only integer value, please, try again: ";
+
+			return (get_num());
+		}
+
+		if (num < 0 || num > 43307) { //При значениях больше 43307 можно поймать segfautl.
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "Too big value, please, try again: ";
+
+			return (get_num());
+		}
+
+		if (num - round(num) != 0) {
+			cin.clear();
+			cin.ignore(1000, '\n');
+			cout << "Only integer value, please, try again: ";
+
+			return (get_num());
+		}
+		
+		return (num);
+	} while(1);
+}
+
+int main()
+{
+	cout << "Enter your integer for `n`: ";
+
+	double n = get_num();
+	double result = calculate_row_y(n);
+
+	cout << endl << "The result of y(n): " << result << endl;
+
+	return (0);
+}
